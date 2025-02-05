5c91625dd108b238d39b8a60c5ce339a4a2c112f
Josh King <162310653+jjjmdev@users.noreply.github.com>
2025-02-05 16:00:13 +0800

    Add test: Ship instance has the correct length property

--- src/js-modules/Ship.test.js
+++ src/js-modules/Ship.test.js
@@ -1,7 +1,14 @@
 import Ship from "./Ship.js"
 
 describe("Ship class", () => {
+	const shipLength = 4
+	const ship = new Ship(shipLength)
+
 	it("is defined", () => {
 		expect(Ship).toBeDefined()
+	})
+
+	it("has a length", () => {
+		expect(ship.length).toBe(shipLength)
 	})
 })
