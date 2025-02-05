864921010805f79d8859a38254cd5756725cf95d
Josh King <162310653+jjjmdev@users.noreply.github.com>
2025-02-05 16:48:40 +0800

    Add test: Gameboard instances have size properties

--- src/js-modules/logic/Gameboard.test.js
+++ src/js-modules/logic/Gameboard.test.js
@@ -1,7 +1,17 @@
 import Gameboard from "./Gameboard.js"
 
 describe("Gameboard class", () => {
+	const nCols = 5
+	const nRows = 10
+	const gameboard = new Gameboard(nCols, nRows)
+
 	it("is defined", () => {
 		expect(Gameboard).toBeDefined()
+	})
+
+	it("has a size", () => {
+		expect(gameboard.size).toBe([nCols, nRows])
+		expect(gameboard.nCols).toBe(nCols)
+		expect(gameboard.nRows).toBe(nRows)
 	})
 })
