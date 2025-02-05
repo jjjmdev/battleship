cbdb543b996b0e7ea5c86ed37d23983f9b3b6aae
Josh King <162310653+jjjmdev@users.noreply.github.com>
2025-02-05 18:35:22 +0800

    Make the Gameboard instances be able to determine whether a ship can be placed in a given position

--- src/js-modules/logic/Gameboard.js
+++ src/js-modules/logic/Gameboard.js
@@ -1,6 +1,13 @@
 import Cell from "./Cell.js"
 import Ship from "./Ship.js"
 
+const directionDisplacement = {
+	N: [0, -1],
+	E: [1, 0],
+	S: [0, 1],
+	W: [-1, 0],
+}
+
 export default class Gameboard {
 	#nCols
 	#nRows
@@ -60,5 +67,28 @@ export default class Gameboard {
 
 	hasShip(name) {
 		return this.#fleet.has(name)
+	}
+
+	canPlaceShip(name, [cStart, rStart], direction) {
+		const ship = this.#fleet.get(name)
+
+		// Compute the end
+		const [cDisp, rDisp] = directionDisplacement[direction]
+		const [cEnd, rEnd] = [
+			cStart + cDisp * (ship.length - 1),
+			rStart + rDisp * (ship.length - 1),
+		]
+
+		console.log(cStart, rStart, cEnd, rEnd)
+
+		// If the start or end is not in board, return false
+		if (
+			!this.isValidCell([cStart, rStart]) ||
+			!this.isValidCell([cEnd, rEnd])
+		) {
+			return false
+		}
+
+		return true
 	}
 }
