22c130463be5ea312c2aff1fb4020ddd7d8fa246
Josh King <162310653+jjjmdev@users.noreply.github.com>
2025-02-05 16:30:56 +0800

    Add test: Cell instances can be occupied by a ship
    You can place a ship, test if a ship is present
    and get which ship occupies the cell.

--- src/js-modules/logic/Cell.test.js
+++ src/js-modules/logic/Cell.test.js
@@ -1,8 +1,10 @@
 import Cell from "./Cell.js"
+import Ship from "./Ship.js"
 
 describe("Cell class", () => {
 	const coords = [4, 6]
 	const cell = new Cell(coords)
+	const ship = new Ship(4)
 
 	it("is defined", () => {
 		expect(Cell).toBeDefined()
@@ -12,5 +14,13 @@ describe("Cell class", () => {
 		expect(cell.coords).toBe(coords)
 		expect(cell.x).toBe(coords[0])
 		expect(cell.y).toBe(coords[1])
+	})
+
+	it("can be occupied by a ship", () => {
+		expect(cell.hasShip()).toBeFalsy()
+		expect(cell.getShip()).toBeNull()
+		cell.placeShip(ship)
+		expect(cell.hasShip()).toBeTruthy()
+		expect(cell.getShip()).toBe(ship)
 	})
 })
