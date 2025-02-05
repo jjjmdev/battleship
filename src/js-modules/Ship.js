9d81df8562e15df4c5af91db1fa778271b5ee91a
Josh King <162310653+jjjmdev@users.noreply.github.com>
2025-02-05 16:02:43 +0800

    Add hits property (getter) initialized to 0 to Ship class definition

--- src/js-modules/Ship.js
+++ src/js-modules/Ship.js
@@ -1,11 +1,17 @@
 export default class Ship {
 	#length
+	#hits
 
 	constructor(length) {
 		this.#length = length
+		this.#hits = 0
 	}
 
 	get length() {
 		return this.#length
+	}
+
+	get hits() {
+		return this.#hits
 	}
 }
