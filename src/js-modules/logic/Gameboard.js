b36c44d5c119e1b5635fc149be574ad5db765e55
Josh King <162310653+jjjmdev@users.noreply.github.com>
2025-02-05 16:56:46 +0800

    Add possibility to retrieve Cell instances from Gameboard class

--- src/js-modules/logic/Gameboard.js
+++ src/js-modules/logic/Gameboard.js
@@ -1,10 +1,22 @@
+import Cell from "./Cell.js"
+
 export default class Gameboard {
 	#nCols
 	#nRows
+	#cells
 
 	constructor(nCols, nRows = nCols) {
 		this.#nCols = nCols
 		this.#nRows = nRows
+
+		this.#cells = []
+		for (let c = 0; c < nCols; c++) {
+			this.#cells.push([])
+			for (let r = 0; r < nRows; r++) {
+				const cell = new Cell([c, r])
+				this.#cells[c].push(cell)
+			}
+		}
 	}
 
 	get size() {
@@ -17,5 +29,9 @@ export default class Gameboard {
 
 	get nRows() {
 		return this.#nRows
+	}
+
+	getCell([c, r]) {
+		return this.#cells[c][r]
 	}
 }
