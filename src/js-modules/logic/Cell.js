497ecf217d85d658647800ad58a5c5e2f27ac5cb
Josh King <162310653+jjjmdev@users.noreply.github.com>
2025-02-05 16:29:03 +0800

    Add coordinate propreties definition to Cell class

--- src/js-modules/logic/Cell.js
+++ src/js-modules/logic/Cell.js
@@ -1,1 +1,19 @@
-export default class Cell {}
+export default class Cell {
+	#coords
+
+	constructor(coords) {
+		this.#coords = coords
+	}
+
+	get coords() {
+		return this.#coords
+	}
+
+	get x() {
+		return this.#coords[0]
+	}
+
+	get y() {
+		return this.#coords[1]
+	}
+}
