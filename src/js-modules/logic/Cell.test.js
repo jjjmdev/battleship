b06be4825f67e3864e59c3d3dc6d81fb9921ecaf
Josh King <162310653+jjjmdev@users.noreply.github.com>
2025-02-05 16:17:55 +0800

    Add test: Cell instances have coordinates properties

--- src/js-modules/logic/Cell.test.js
+++ src/js-modules/logic/Cell.test.js
@@ -1,7 +1,16 @@
 import Cell from "./Cell.js"
 
 describe("Cell class", () => {
+	const coords = [4, 6]
+	const cell = new Cell(coords)
+
 	it("is defined", () => {
 		expect(Cell).toBeDefined()
+	})
+
+	it("has some coordinates", () => {
+		expect(cell.coords).toBe(coords)
+		expect(cell.x).toBe(coords[0])
+		expect(cell.y).toBe(coords[1])
 	})
 })
