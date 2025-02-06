9fbde546b57cc5b3de70e3aa19f8aa45a9c9defe
Josh King <162310653+jjjmdev@users.noreply.github.com>
2025-02-06 18:53:38 +0800

    Add test: Player instances have a name that can be retrieved

--- src/js-modules/logic/Player.test.js
+++ src/js-modules/logic/Player.test.js
@@ -4,4 +4,11 @@ describe("Player class", () => {
 	it("is defined", () => {
 		expect(Player).toBeDefined()
 	})
+
+	const playerName = "Captain X"
+	const player = new Player(playerName)
+
+	it("has a name", () => {
+		expect(player.name).toBe(playerName)
+	})
 })
