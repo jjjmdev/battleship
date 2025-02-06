211ebe65975446ace6dcd1268bf95c253aa9499c
Josh King <162310653+jjjmdev@users.noreply.github.com>
2025-02-06 19:12:08 +0800

    Initialize Player instances with a not deployed fleet that can be retrieve

--- src/js-modules/logic/Player.js
+++ src/js-modules/logic/Player.js
@@ -3,10 +3,17 @@ import Gameboard from "./Gameboard"
 export default class Player {
 	#name
 	#gameboard
+	#fleet
 
-	constructor(name, nCols = 10, nRows = nCols) {
+	constructor(name, fleet, nCols = 10, nRows = nCols) {
 		this.#name = name
 		this.#gameboard = new Gameboard(nCols, nRows)
+
+		this.#fleet = fleet
+
+		this.#fleet.forEach(([name, length]) =>
+			this.#gameboard.addShip(name, length)
+		)
 	}
 
 	get name() {
@@ -15,5 +22,9 @@ export default class Player {
 
 	get gameboard() {
 		return this.#gameboard
+	}
+
+	get fleet() {
+		return this.#fleet.keys()
 	}
 }
