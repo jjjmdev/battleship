3e5552dc2e333ebd80612b03b807161994790d8f
Josh King <162310653+jjjmdev@users.noreply.github.com>
2025-02-06 18:58:23 +0800

    Add Gameboard getter to Player class: the size is set in the constructor

--- src/js-modules/logic/Player.js
+++ src/js-modules/logic/Player.js
@@ -1,11 +1,19 @@
+import Gameboard from "./Gameboard"
+
 export default class Player {
 	#name
+	#gameboard
 
-	constructor(name) {
+	constructor(name, nCols = 10, nRows = nCols) {
 		this.#name = name
+		this.#gameboard = new Gameboard(nCols, nRows)
 	}
 
 	get name() {
 		return this.#name
+	}
+
+	get gameboard() {
+		return this.#gameboard
 	}
 }
