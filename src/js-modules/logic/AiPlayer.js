edeaf4e6704f2f3d7c2da528616655e9958fda34
Josh King <162310653+jjjmdev@users.noreply.github.com>
2025-02-07 15:58:12 +0800

    Add method to AiPlayer class to choose a valid opponent target cell for the next attack

--- src/js-modules/logic/AiPlayer.js
+++ src/js-modules/logic/AiPlayer.js
@@ -1,6 +1,9 @@
 import Player from "./Player.js"
+import { randomInt } from "../utils/math.js"
 
 export default class AiPlayer extends Player {
+	#possibleTargets
+
 	constructor(
 		name,
 		fleet = Player.defaultFleet,
@@ -8,5 +11,23 @@ export default class AiPlayer extends Player {
 		nRows = nCols
 	) {
 		super(name, fleet, nCols, nRows)
+
+		this.#initPossibleTargets()
+	}
+
+	#initPossibleTargets() {
+		this.#possibleTargets = new Set(
+			this.gameboard.cells.flat().map((cell) => cell.coords)
+		)
+	}
+
+	getOpponentTargetCellCoords() {
+		return this.#getOpponentTargetCellCoordsRandom()
+	}
+
+	#getOpponentTargetCellCoordsRandom() {
+		return Array.from(this.#possibleTargets)[
+			randomInt(0, this.#possibleTargets.size - 1)
+		]
 	}
 }
