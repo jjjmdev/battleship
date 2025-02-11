f3c923a2c325025681760756a6344c5093f90259
Josh King <162310653+jjjmdev@users.noreply.github.com>
2025-02-11 14:07:36 +0800

    Make GameController class initalize players (human or AI) that can be retrieved

--- src/js-modules/logic/GameController.js
+++ src/js-modules/logic/GameController.js
@@ -1,1 +1,28 @@
-export default class GameController {}
+import Player from "./Player.js"
+import AiPlayer from "./AiPlayer.js"
+
+export default class GameController {
+	#player1
+	#player2
+
+	constructor(player1Name, player2Name, versusAi = true) {
+		this.#player1 = this.#initPlayer(player1Name)
+		this.#player2 = this.#initPlayer(player2Name, versusAi)
+	}
+
+	get player1() {
+		return this.#player1
+	}
+
+	get player2() {
+		return this.#player2
+	}
+
+	#initPlayer(playerName, isAi = false) {
+		if (isAi) {
+			return new AiPlayer(playerName)
+		} else {
+			return new Player(playerName)
+		}
+	}
+}
