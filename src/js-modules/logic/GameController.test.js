a89d53af6e189c33265708227db6a4f014326a1b
Josh King <162310653+jjjmdev@users.noreply.github.com>
2025-02-11 14:07:09 +0800

    Add test: GameController class initializes players (human or AI) that can be retrieved

--- src/js-modules/logic/GameController.test.js
+++ src/js-modules/logic/GameController.test.js
@@ -4,4 +4,27 @@ describe("GameController class", () => {
 	it("is defined", () => {
 		expect(GameController).toBeDefined()
 	})
+
+	const player1Name = "player X"
+	const player2Name = "player Y"
+	const player2AIName = "player AI"
+
+	const gameController2Players = new GameController(
+		player1Name,
+		player2Name,
+		false
+	)
+	const gameController1Player = new GameController(
+		player1Name,
+		player2AIName,
+		true
+	)
+
+	it("initialize players (human or AI) that can be retrieved", () => {
+		expect(gameController2Players.player1.name).toBe(player1Name)
+		expect(gameController2Players.player2.name).toBe(player2Name)
+
+		expect(gameController1Player.player1.name).toBe(player1Name)
+		expect(gameController1Player.player2.name).toBe(player2AIName)
+	})
 })
