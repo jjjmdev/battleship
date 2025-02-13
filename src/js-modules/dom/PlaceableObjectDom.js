3a2bccf52d8124068b01c5046a161a9c4b441fe2
Josh King <162310653+jjjmdev@users.noreply.github.com>
2025-02-13 12:53:02 +0800

    Add PlaceableObjectDom module to place object on grid containers
    This module defines the PlaceableObjectDom class which initilizes a div
    that set up the occupied row/columns grid element and other styles to
    make them occupy the coordinates specified in the constructor.
    
    This will be used for placing ships, and hit/miss marks on the
    gameboard. In particular, classes extending this class will be
    created to add specific properties.

---
+++ src/js-modules/dom/PlaceableObjectDom.js
@@ -0,0 +1,52 @@
+import { initDiv } from "../utils/domComponents.js"
+
+export default class PlaceableObjectDom {
+	#div
+	static blockName = "placeable-object"
+	static zIndex = 0
+
+	constructor(cellsCoords) {
+		this.#div = this.#initObjectDiv(cellsCoords)
+	}
+
+	get div() {
+		return this.#div
+	}
+
+	#initObjectDiv(cellsCoords) {
+		const div = initDiv(this.constructor.blockName)
+		this.#setGridLocation(div, cellsCoords)
+		return div
+	}
+
+	#setGridLocation(div, cellsCoords) {
+		const minCol = cellsCoords.reduce(
+			(min, item) => (item[0] < min ? item[0] : min),
+			cellsCoords[0][0]
+		)
+
+		const minRow = cellsCoords.reduce(
+			(min, item) => (item[1] < min ? item[1] : min),
+			cellsCoords[0][1]
+		)
+
+		const maxCol = cellsCoords.reduce(
+			(max, item) => (item[0] > max ? item[0] : max),
+			cellsCoords[0][0]
+		)
+
+		const maxRow = cellsCoords.reduce(
+			(max, item) => (item[1] > max ? item[1] : max),
+			cellsCoords[0][1]
+		)
+
+		// console.log(minCol, maxCol, minRow, maxRow)
+
+		div.style.gridColumn = `${minCol + 1} / ${maxCol + 2}`
+		div.style.gridRow = `${minRow + 1} / ${maxRow + 2}`
+		div.style.width = "100%"
+		div.style.height = "100%"
+		div.style.zIndex = this.constructor.zIndex
+		div.style.aspectRatio = `${maxCol - minCol + 1} / ${maxRow - minRow + 1}`
+	}
+}
