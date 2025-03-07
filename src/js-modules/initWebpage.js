c237f2278e1504245e05d9dcc411ba2a288b0f2d
Josh King <162310653+jjjmdev@users.noreply.github.com>
2025-03-07 08:14:00 +0800

    Show the Home page at startup

--- src/js-modules/initWebpage.js
+++ src/js-modules/initWebpage.js
@@ -2,7 +2,6 @@ import GameViewDom from "./dom/GameViewDom.js"
 import GameEndViewDom from "./dom/GameEndViewDom.js"
 import HomeViewDom from "./dom/HomeViewDom.js"
 import PlayersNameViewDom from "./dom/PlayersNameViewDom.js"
-import GameController from "./logic/GameController.js"
 import PubSub from "pubsub-js"
 import { pubSubTokens } from "./pubSubTokens.js"
 import { removeDescendants } from "./utils/domUtilities.js"
@@ -10,16 +9,12 @@ import { removeDescendants } from "./utils/domUtilities.js"
 const container = document.body
 
 export default function initWebpage() {
-	const player1Name = "Captain X"
-	const player2Name = "Captain Y"
-	const versusAi = true
-
 	PubSub.subscribe(pubSubTokens.showGameView, renderGameViewDom)
 	PubSub.subscribe(pubSubTokens.showGameEndView, renderGameViewEndDom)
 	PubSub.subscribe(pubSubTokens.showHomeView, renderHomeViewDom)
 	PubSub.subscribe(pubSubTokens.showPlayersNameView, renderPlayersNameViewDom)
 
-	new GameController(player1Name, player2Name, versusAi)
+	renderHomeViewDom()
 }
 
 function renderGameViewDom(token, { player1, player2 }) {
