29b20cb1a86ce4a6cd1a5c9017ea28e8020d94f0
Josh King <162310653+jjjmdev@users.noreply.github.com>
2025-03-11 14:20:24 +0800

    Refactor AiPlayer class to being able to handle different skill levels
    The level is passed to the constructor through a string.
    
    So far, the "random" AI player is still the only possible
    skill level, and is the default skill level.

--- src/js-modules/logic/AiPlayer.js
+++ src/js-modules/logic/AiPlayer.js
@@ -1,18 +1,26 @@
 import Player from "./Player.js"
 import { randomInt } from "../utils/math.js"
 
+const defaultSkills = "random"
+
 export default class AiPlayer extends Player {
 	#possibleTargets
+	#skills
+	#getOpponentTargetCellCoords // methods initialized based on the #skills
+	#applyPostAttackActions // methods initialized based on the #skills
 
 	constructor(
 		name,
 		fleet = Player.defaultFleet,
 		nCols = Player.defaultGameboardSize,
-		nRows = nCols
+		nRows = nCols,
+		skills = defaultSkills
 	) {
 		super(name, fleet, nCols, nRows)
 
 		this.#initPossibleTargets()
+		this.#skills = skills
+		this.#initPlayerSkills()
 	}
 
 	#initPossibleTargets() {
@@ -21,10 +29,28 @@ export default class AiPlayer extends Player {
 		)
 	}
 
-	getOpponentTargetCellCoords() {
-		return this.#getOpponentTargetCellCoordsRandom()
+	#initPlayerSkills() {
+		if (this.#skills == "random") {
+			this.#getOpponentTargetCellCoords =
+				this.#getOpponentTargetCellCoordsRandom
+			this.#applyPostAttackActions = this.#applyPostAttackActionsRandom
+		}
 	}
 
+	getOpponentTargetCellCoords() {
+		// this is a wrapper and allows to use different strategies in the future
+		// implementations using the same interface: todo
+		return this.#getOpponentTargetCellCoords()
+	}
+
+	applyPostAttackActions(cellCoords, otherData = {}) {
+		// this is a wrapper and allows to use different strategies in the future
+		// implementations using the same interface: todo
+		// otherData is set as argument for future improvements
+		return this.#applyPostAttackActions(cellCoords, otherData)
+	}
+
+	/* random strategy */
 	#getOpponentTargetCellCoordsRandom() {
 		if (this.#possibleTargets.size === 0) {
 			throw new Error("There are no possible opponent targets")
@@ -33,10 +59,6 @@ export default class AiPlayer extends Player {
 		return Array.from(this.#possibleTargets)[
 			randomInt(0, this.#possibleTargets.size - 1)
 		]
-	}
-
-	applyPostAttackActions(cellCoords, otherData = {}) {
-		return this.#applyPostAttackActionsRandom(cellCoords)
 	}
 
 	#applyPostAttackActionsRandom(cellCoords) {
