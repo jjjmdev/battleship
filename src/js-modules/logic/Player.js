7582c29123f863974a8563e97787d8b4d6c5e295
Josh King <162310653+jjjmdev@users.noreply.github.com>
2025-03-20 17:29:22 +0800

    Add repeatRandomShipsPlacement() method to Player class to randomly place again the deployed fleet in the board

--- src/js-modules/logic/Player.js
+++ src/js-modules/logic/Player.js
@@ -55,4 +55,14 @@ export default class Player {
 			this.#gameboard.placeShip(name, [cStart, rStart], direction)
 		})
 	}
+
+	repeatRandomShipsPlacement() {
+		// reset the deployed fleet
+		const deployedFleet = [...this.#gameboard.deployedFleet]
+		deployedFleet.forEach((name) => {
+			this.#gameboard.resetShip(name)
+		})
+
+		this.randomShipsPlacement()
+	}
 }
